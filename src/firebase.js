82edfa9a5ebaf68dfed234b744c3fda3c1b53971
Renato Pavlic <renato.poslovni@gmail.com>
2021-02-13 21:04:50 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDXbFPtSORd-N3V41AUmr_hf9_H7dI4iE",
+  authDomain: "netflix-5fd0a.firebaseapp.com",
+  projectId: "netflix-5fd0a",
+  storageBucket: "netflix-5fd0a.appspot.com",
+  messagingSenderId: "613996141287",
+  appId: "1:613996141287:web:359e20b4ca454d134b6042",
+  measurementId: "G-J5XW21TJZM"
+};
