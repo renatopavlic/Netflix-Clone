33563a0453d6e2fe37dc49066fd3c18bfa802444
Renato Pavlic <renato.poslovni@gmail.com>
2021-02-19 21:11:58 +0100

    ready for hosting

--- src/Row.js
+++ src/Row.js
@@ -28,14 +28,18 @@ function Row( { title, fetchUrl, isLargeRow = false } ) {
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
-        {movies.map( movie => (
-          <img 
+        {movies.map(
+          (movie) => 
+          ((isLargeRow && movie.poster_path) ||
+           (!isLargeRow && movie.backdrop_path)) && (
+            <img 
             key={movie.id}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`} alt={movie.name} />
-        ))}
+           )
+        )}
       </div>
     </div>
   )
